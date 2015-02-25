32be64e7b476e4ef802dfe25441f052350a69230
Chocobo1 <Chocobo1@users.noreply.github.com>
2015-02-25 19:04:28 +0800

    formatting

--- src/stdafx.h
+++ src/stdafx.h
@@ -4,7 +4,7 @@
 #include <ctime>
 #include <future>
 
-#include <Shlwapi.h>	// foobar2000 SDK
+#include <Shlwapi.h>	// for foobar2000 SDK
 
 #include "../lib/foobar2000_sdk/foobar2000/SDK/foobar2000.h"
 #include "../lib/tinyxml2/tinyxml2.h"
