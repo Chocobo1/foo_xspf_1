3e9ee167cdeda89db72b4e57f867f03cfcb92929
Chocobo1 <Chocobo1@users.noreply.github.com>
2015-02-25 21:26:53 +0800

    rename

--- src/main.h
+++ src/main.h
@@ -14,4 +14,4 @@ class xspf : public playlist_loader
 		void write( const char *p_path , const service_ptr_t<file> &p_file , metadb_handle_list_cref p_data , abort_callback &p_abort );
 };
 // instance
-playlist_loader_factory_t<xspf> xspf_main_1;
+playlist_loader_factory_t<xspf> xspf_main;
